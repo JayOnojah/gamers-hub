55c3245e3703461e1967bd0eab7829e9d0a9b9a8
Jeremiah Onojah <jayonojah@gmail.com>
2024-03-18 10:57:46 +0100

    Updated Favicon

--- app/(dashboard)/u/[username]/_components/navbar/logo.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -14,7 +14,7 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
         <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image src="/spooky.svg" alt="Gamers Hub" height="32" width="32" />
+          <Image src="/logo.png" alt="Gamers Hub" height="32" width="32" />
         </div>
         <div className={cn('hidden lg:block', font.className)}>
           <p className="text-xl font-semibold">Gamers Hub</p>
